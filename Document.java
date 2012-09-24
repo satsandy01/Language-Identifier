011d40134739a9e19f7d78c410af89cc1509cf15
Ayush Jaiswal <mail.ayush.jaiswal@gmail.com>
2012-09-24 17:25:53 +0530

    Corrected a faulty method

--- Document.java
+++ Document.java
@@ -49,7 +49,8 @@ public class Document
 			content=content.replace(')', ' ');
 			content=content.replace('&', ' ');
 			content=content.replace('/', ' ');
-			content=content.replace("  ", " ");
+			content=content.replaceAll("\t+", " ");
+			content=content.replaceAll(" +", " ");
 			System.out.print("Done\n");
 			words = new ArrayList<String>(Arrays.asList(content.split(" ")));
 			if(words.size()==0 || (words.size()==1 && words.get(0) ==""))
